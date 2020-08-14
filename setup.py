2205e4664ec764e1e61ed52d95eebb606ea12bf5
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-08-14 22:38:39 +0900

    Order keys by alphabet

--- setup.py
+++ setup.py
@@ -10,21 +10,8 @@ def main():
         readme = file.read()
 
     setup(
-        name="yamldataclassconfig",
-        version="1.4.0",
-        description="This project helps you to import config file writen by YAML to Python data class.",
-        long_description=readme,
-        long_description_content_type="text/markdown",
         author="Yukihiko Shinoda",
         author_email="yuk.hik.future@gmail.com",
-        packages=find_packages(exclude=("tests*", "myproduct*", "yourproduct*")),
-        package_data={"yamldataclassconfig": ["py.typed"]},
-        python_requires=">=3.7",
-        install_requires=["dataclasses-json", "pyyaml"],
-        dependency_links=[],
-        setup_requires=["pytest-runner"],
-        url="https://github.com/yukihiko-shinoda/yaml-dataclass-config",
-        keywords="yaml dataclass dataclasses config",
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
@@ -36,6 +23,19 @@ def main():
             "Programming Language :: Python :: 3.8",
             "Topic :: Software Development :: Quality Assurance",
         ],
+        dependency_links=[],
+        description="This project helps you to import config file writen by YAML to Python data class.",
+        install_requires=["dataclasses-json", "pyyaml"],
+        keywords="yaml dataclass dataclasses config",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        name="yamldataclassconfig",
+        packages=find_packages(exclude=("tests*", "myproduct*", "yourproduct*")),
+        package_data={"yamldataclassconfig": ["py.typed"]},
+        python_requires=">=3.7",
+        setup_requires=["pytest-runner"],
+        url="https://github.com/yukihiko-shinoda/yaml-dataclass-config",
+        version="1.4.0",
     )
 
 
